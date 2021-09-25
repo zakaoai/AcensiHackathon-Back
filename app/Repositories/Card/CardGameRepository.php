b868a2df5c6157d04e0f8c2b8390476cd5807e70
Kanbatsu <ludwig.secheresse@gmail.com>
2021-09-25 05:11:53 +0200

    Add CadGames Repo & Service

---
+++ app/Repositories/Card/CardGameRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories\Card;
+
+
+use App\Models\CardGame;
+
+class CardGameRepository
+{
+    private CardGame $entity;
+
+    public function __construct()
+    {
+        $this->entity = app(CardGame::class);
+    }
+
+    public function findOrFail(string $id)
+    {
+        return $this->entity->newQuery()->findOrFail($id);
+    }
+
+    public function all()
+    {
+        return $this->entity->newQuery()->get();
+    }
+
+    public function create(array $attributes)
+    {
+        return $this->entity->newQuery()->create($attributes);
+    }
+
+    public function findByGame(int $id)
+    {
+        return $this->entity->newQuery()->where('game_id', $id)->get();
+    }
+}
---
+++ app/Services/Card/CardGameService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services\Card;
+
+use App\Models\Card;
+use App\Repositories\Card\CardGameRepository;
+use App\Repositories\Card\CardRepository;
+
+class CardGameService
+{
+    private CardGameRepository $cardGameRepository;
+
+    public function __construct(CardGameRepository $cardGameRepository)
+    {
+        $this->cardGameRepository = $cardGameRepository;
+    }
+
+    public function findOrFail(string $id)
+    {
+        return $this->cardGameRepository->findOrFail($id);
+    }
+
+    public function all()
+    {
+        return $this->cardGameRepository->all();
+    }
+
+    public function allWith()
+    {
+        return $this->cardGameRepository->allWith();
+    }
+
+    public function create(array $attributes)
+    {
+        return $this->cardGameRepository->create($attributes);
+    }
+
+    public function findByGame(int $id)
+    {
+        return $this->cardGameRepository->findByGame($id);
+    }
+}
